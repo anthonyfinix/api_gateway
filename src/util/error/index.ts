b6e6159461245479cd0f9dbb9a8f71b6d486725c
Anthony Finix <anthonyfinix@gmail.com>
2021-07-09 00:00:26 +0530

    added not authorized error class

--- src/util/error/index.ts
+++ src/util/error/index.ts
@@ -10,24 +10,26 @@ export class GeneralError extends Error {
     constructor({ message, status, details }: IError) {
         super();
         this.status = this.setStatus(status);
-        this.message = this.setMessage(message);
         this.details = this.setDetails(details);
+        this.message = this.setMessage(message);
     }
     protected setStatus(status?: number): number {
         if (status) return status;
-        if (this instanceof InternalError) return 500;
-        if (this instanceof BadRequest) return 400;
         if (this instanceof NotFound) return 404;
+        if (this instanceof BadRequest) return 400;
+        if (this instanceof NotAuthorized) return 401;
+        if (this instanceof InternalError) return 500;
         return 400
     }
     protected setMessage(message?: string): string {
         if (message) return message;
-        if (this instanceof InternalError) return 'There was an internal Error';
-        if (this instanceof BadRequest) return 'Bad Request';
         if (this instanceof NotFound) return 'Not Found';
+        if (this instanceof BadRequest) return 'Bad Request';
+        if (this instanceof NotAuthorized) return 'Not Authorized';
+        if (this instanceof InternalError) return 'There was an internal Error';
         return "there was an internal error"
     }
-    protected setDetails(details?: Array<unknown | Error>):Array<unknown | Error> {
+    protected setDetails(details?: Array<unknown | Error>): Array<unknown | Error> {
         if (details) return details;
         return []
     }
@@ -40,7 +42,8 @@ export class GeneralError extends Error {
     }
 }
 
+export class NotFound extends GeneralError { }
 export class BadRequest extends GeneralError { }
 export class InternalError extends GeneralError { }
-export class NotFound extends GeneralError { }
+export class NotAuthorized extends GeneralError { }
 
