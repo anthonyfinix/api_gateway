4be2818ef65fa0c22321d9cad1a95d0b474a5a20
Anthony Finix <anthonyfinix@gmail.com>
2021-07-09 13:47:09 +0530

    refactored add user, added role validaton and added multer as route middle for post user

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -7,12 +7,13 @@ import user from './user/index.';
 import expressJson from '../middleware/expressJson';
 import urlEncoded from '../middleware/urlEncoded';
 import addUser from './user/addUser';
+import multer from '../middleware/multer';
 // entities
 
 const router: Router = express.Router();
-router.get('/',(req,res)=>{res.send('api_gateway')})
-router.post('/login',expressJson(),urlEncoded(), login);
-router.post('/register',expressJson(),urlEncoded(), addUser)
+router.get('/', (req, res) => { res.send('api_gateway') })
+router.post('/login', expressJson(), urlEncoded(), login);
+router.post('/register', expressJson(), multer().single('profile'), urlEncoded(), addUser)
 router.use('/user', user)
 router.use(not_found);
 router.use(error);
--- src/routes/user/addUser.ts
+++ src/routes/user/addUser.ts
@@ -1,26 +1,42 @@
 import { NextFunction, Request, Response } from "express";
 import { userHttp } from "../../util/axios/user";
-import { InternalError, NotAuthorized } from "../../util/error";
+import { BadRequest, InternalError, NotAuthorized } from "../../util/error";
 import FormData from "form-data";
+import { roleHttp } from "../../util/axios/role";
 
 
 export default async (req: any, res: Response, next: NextFunction) => {
     if (!req.user) return next(new NotAuthorized({}))
     try {
-        let form = new FormData();
-        let fileOptions = { ...req.file }
-        for (let key in req.body) {
-            form.append(key, req.body[key])
-        };
-        delete fileOptions.fieldname;
-        delete fileOptions.buffer;
-        fileOptions.filename = `${req.username}.${req.file.originalname.split('.')[1]}`;
+        let roleResponse = await roleHttp.get('/', { params: { name: req.body.role } })
+        if (roleResponse.data.result.length != 1) return next(new BadRequest({ message: 'no role found with provided name' }))
+        let form = getFormData(req.body);
+        let fileOptions: any = getFileOption(req.file);
+        fileOptions.filename = getFileName(req.username, fileOptions.originalname);
         form.append('profile', req.file.buffer, fileOptions);
         let response = await userHttp.post('/user/', form, { headers: form.getHeaders() });
         let { data } = response;
         return res.status(200).json(data);
     } catch (e) {
+        console.log(e);
         if (e.response) return res.status(e.response.status).json(e.response.data);
         return next(new InternalError({ message: 'there was an error with user service response' }))
     }
+}
+
+const getFormData = (body: any) => {
+    let form = new FormData();
+    for (let key in body) form.append(key, body[key]);
+    return form;
+}
+
+const getFileOption = (fileDetails: any) => {
+    let fileOptions = { ...fileDetails };
+    delete fileOptions.fieldname;
+    delete fileOptions.buffer;
+    return fileOptions;
+}
+
+const getFileName = (username: string, originalname: string): string => {
+    return `${username}.${originalname.split('.')[1]}`;
 }
